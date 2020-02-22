0e320c94359b7ec2589f962d3b14684bf6ac25f4
Zaid Alhir <zaidalhir@yahoo.com>
2020-02-22 01:42:49 -0600

    crating app.js and routs,adding pict

---
+++ pupster/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Rout } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
+import './App.css';
+
+
+
+
+export default App;
